a4ac2d715ef8d79ae1247d88756cc203a6711872
NathaliaMachado <nathaliamachd@gmail.com>
2019-07-08 19:19:06 -0400

    router and navigation support

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
-import AutorBox from './FormularioAutor';
+import { Link } from 'react-router-dom';
+
 
 class App extends Component {
-
 
   render() {
 
@@ -20,23 +20,17 @@ class App extends Component {
                <a className="pure-menu-heading" href="#">Company</a>
 
               <ul className="pure-menu-lista">
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Author</a></li>
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Book</a></li>
+                <li className="pure-menu-item"><Link to="/" className="pure-menu-link">Home</Link></li>
+                <li className="pure-menu-item"><Link to="/autor" className="pure-menu-link">Author</Link></li>
+                <li className="pure-menu-item"><Link to="/livro" className="pure-menu-link">Book</Link></li>
               </ul>
             </div>
           </div>
 
           <div id="main">
-            <div className="header">
-                <h1>Author's register</h1>
-            </div>
-            <div className="content" id="content">
-               <AutorBox/>
-            </div>  
+            {this.props.children} 
           </div>
         </div>
-        
     );
   }
 }
